babcfbc49df529896f24d003c71158ed655da41d
Radu Albastroiu <albastroiuradug@gmail.com>
2020-06-16 12:28:42 +0300

    Add hello world

---
+++ SimpleHandGestureRecognition/handGestureRecognition.js
@@ -0,0 +1,5 @@
+const test = () => {
+  console.log('Hello World');
+};
+
+module.exports = test;
---
+++ index.js
@@ -0,0 +1,3 @@
+let test = require('./SimpleHandGestureRecognition/handGestureRecognition');
+
+test();
