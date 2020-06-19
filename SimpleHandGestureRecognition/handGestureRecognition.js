471cc7df5bea1a92592ef830a79c112ecedf758c
Radu Albastroiu <albastroiuradug@gmail.com>
2020-06-19 16:19:29 +0300

    Added a hand mask and getting the hull contour

--- SimpleHandGestureRecognition/handGestureRecognition.js
+++ SimpleHandGestureRecognition/handGestureRecognition.js
@@ -1,3 +1,67 @@
+// segmenting by skin color (has to be adjusted)
+const skinColorUpper = (hue) => new cv.Vec(hue, 0.8 * 255, 0.6 * 255);
+const skinColorLower = (hue) => new cv.Vec(hue, 0.1 * 255, 0.05 * 255);
+
+const makeHandMask = (img) => {
+  // filter by skin color
+  const imgHLS = img.cvtColor(cv.COLOR_BGR2HLS);
+  const rangeMask = imgHLS.inRange(skinColorLower(0), skinColorUpper(15));
+
+  // remove noise
+  const blurred = rangeMask.blur(new cv.Size(10, 10));
+  const thresholded = blurred.threshold(200, 255, cv.THRESH_BINARY);
+
+  return thresholded;
+};
+
+const getHandContour = (handMask) => {
+  const contours = handMask.findContours(
+    cv.RETR_EXTERNAL,
+    cv.CHAIN_APPROX_SIMPLE
+  );
+  // largest contour
+  return contours.sort((c0, c1) => c1.area - c0.area)[0];
+};
+
+// get the polygon from a contours hull such that there
+// will be only a single hull point for a local neighborhood
+const getRoughHull = (contour, maxDist) => {
+  // get hull indices and hull points
+  const hullIndices = contour.convexHullIndices();
+  const contourPoints = contour.getPoints();
+  const hullPointsWithIdx = hullIndices.map((idx) => ({
+    pt: contourPoints[idx],
+    contourIdx: idx,
+  }));
+  const hullPoints = hullPointsWithIdx.map((ptWithIdx) => ptWithIdx.pt);
+
+  // group all points in local neighborhood
+  const ptsBelongToSameCluster = (pt1, pt2) => ptDist(pt1, pt2) < maxDist;
+  const { labels } = cv.partition(hullPoints, ptsBelongToSameCluster);
+  const pointsByLabel = new Map();
+  labels.forEach((l) => pointsByLabel.set(l, []));
+  hullPointsWithIdx.forEach((ptWithIdx, i) => {
+    const label = labels[i];
+    pointsByLabel.get(label).push(ptWithIdx);
+  });
+
+  // map points in local neighborhood to most central point
+  const getMostCentralPoint = (pointGroup) => {
+    // find center
+    const center = getCenterPt(pointGroup.map((ptWithIdx) => ptWithIdx.pt));
+    // sort ascending by distance to center
+    return pointGroup.sort(
+      (ptWithIdx1, ptWithIdx2) =>
+        ptDist(ptWithIdx1.pt, center) - ptDist(ptWithIdx2.pt, center)
+    )[0];
+  };
+  const pointGroups = Array.from(pointsByLabel.values());
+  // return contour indices of most central points
+  return pointGroups
+    .map(getMostCentralPoint)
+    .map((ptWithIdx) => ptWithIdx.contourIdx);
+};
+
 const test = () => {
   console.log('Hello World');
 };
