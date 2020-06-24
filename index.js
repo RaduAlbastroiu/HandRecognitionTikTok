b038eb9886c812d4a08be44a62f593fe089eb5cc
Radu Albastroiu <albastroiuradug@gmail.com>
2020-06-24 22:46:44 +0300

    Add video detection

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 let test = require('./SimpleHandGestureRecognition/handGestureRecognition');
 
-require('./webcamfacedetection/faceDetect/webcamFaceDetection');
+//require('./facedetection/faceDetect/webcamFaceDetection');
 
-/*
+require('./facedetection/faceDetect/videoFaceDetection');
+
 let cv = require('opencv4nodejs');
 
+/*
 let img = cv.imread('pug.png');
-cv.imwrite('img.png', img);
+cv.imshow('img', img);
+cv.waitKey(0);
 */
