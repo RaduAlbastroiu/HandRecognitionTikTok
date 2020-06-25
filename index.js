a1f150201cd0f5e5c5abe188078fa98cb4d09169
Radu Albastroiu <albastroiuradug@gmail.com>
2020-06-25 19:15:53 +0300

    Add Tensorflow handpose

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-require('./opencv4nodejs/facedetection/faceDetect/videoFaceDetection');
-
-let cv = require('opencv4nodejs');
-
-/*
-let img = cv.imread('pug.png');
-cv.imshow('img', img);
-cv.waitKey(0);
-*/
+require('./tensorflowMediaPipe/test');
