23d5e92aa0287fd40d1ca425f99a8c17d20730b9
Radu Albastroiu <albastroiuradug@gmail.com>
2020-06-26 12:58:20 +0300

    Add react app with tensorflow

---
+++ tensorflowMediaPipe/tensorflowhandposeapp/src/App.js
@@ -0,0 +1,48 @@
+import React, { useEffect } from 'react';
+
+const handpose = require('@tensorflow-models/handpose');
+const test_img = require('./test_img.png');
+
+async function main() {
+  const model = await handpose.load();
+
+  console.log('HERE');
+
+  // provide img / video
+  const predictions = await model.estimateHands(document.querySelector('img'));
+
+  console.log('Length');
+  console.log(predictions.length);
+  if (predictions.length > 0) {
+    console.log('Length');
+    console.log(predictions.length);
+
+    for (let i = 0; i < predictions.length; i++) {
+      const keypoints = predictions[i].landmarks;
+
+      // Log hand keypoints.
+      for (let i = 0; i < keypoints.length; i++) {
+        const [x, y, z] = keypoints[i];
+        console.log(`Keypoint ${i}: [${x}, ${y}, ${z}]`);
+      }
+    }
+  }
+}
+
+function App() {
+  useEffect(() => {
+    console.log('mounted');
+    main();
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Test</h1>
+        <img src={test_img}></img>
+      </header>
+    </div>
+  );
+}
+
+export default App;
