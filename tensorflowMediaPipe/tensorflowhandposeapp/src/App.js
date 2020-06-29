8b87ee561c66ed48c3c246597ec236f3b8b835c9
Radu Albastroiu <albastroiuradug@gmail.com>
2020-06-29 12:40:02 +0300

    Delete unnecesarry code

--- tensorflowMediaPipe/tensorflowhandposeapp/src/App.js
+++ tensorflowMediaPipe/tensorflowhandposeapp/src/App.js
@@ -9,21 +9,6 @@ import {
   loadRandomForest,
 } from './randomforest/randomForest';
 
-function writeCsv() {
-  console.log('plm');
-  const rows = [
-    ['name1', 'city1', 'some other info'],
-    ['name2', 'city2', 'more info'],
-  ];
-
-  let csvContent = 'data:text/csv;charset=utf-8,';
-
-  rows.forEach(function (rowArray) {
-    let row = rowArray.join(',');
-    csvContent += row + '\r\n';
-  });
-}
-
 function App() {
   let [handGesture, setHandGesture] = React.useState('None');
 
